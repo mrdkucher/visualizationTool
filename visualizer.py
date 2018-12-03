e9011da0b6a6511b2e019e37eabd9ec4a6fd5283
David Kucher <mrdkucher@gmail.com>
2018-12-03 14:18:34 -0500

    Added MST

--- visualizer.py
+++ visualizer.py
@@ -4,11 +4,9 @@ import re
 import sys
 
 def visualizeTour(pointsFile, tourFiles):
-	#, tourFile2):
 	#read in points
 	vtxFile = open(pointsFile, "rb")
-	vtxCount = vtxFile.readline()
-	vtxCount = re.findall('\d+', vtxCount)
+	vtxCount = re.findall('\d+', vtxFile.readline())
 	vtxCount = int(vtxCount[0])
 
 	#create array of vertices
@@ -19,13 +17,10 @@ def visualizeTour(pointsFile, tourFiles):
 		coords = re.findall('\d+', coords)
 		vertices[i] = (coords[0], coords[1])
 
-	#vertices = np.array(vertices)
-	#vtxX, vtxY = vertices.T
-
 	#create a plot
 	tourAxes = np.empty(len(tourFiles), dtype=object)
 
-	tours = np.ndarray(shape=(len(tourFiles),vtxCount), dtype='i')
+	tours = np.ndarray(shape=(len(tourFiles), vtxCount), dtype='i')
 	sortedVertices = np.ndarray(shape=(len(tourFiles), vtxCount + 1), dtype ='i,i')
 	
 	#read in tours and create plots
@@ -36,32 +31,60 @@ def visualizeTour(pointsFile, tourFiles):
 			
 		#create a copy of the vertices in the order of the tour
 		for v in range(vtxCount):
-			tours[i][v] = vtx[v]
-			sortedVertices[i][v] = vertices[tours[i][v]]
+			tours[i, v] = vtx[v]
+			sortedVertices[i, v] = vertices[tours[i, v]]
 		
 		#include a return to the first vertex of the tour
 		sortedVertices[i, vtxCount] = vertices[0]
 		
 		#get the tour in a format that can be plotted
-		#print(sortedVertices[i, 0])
-		#print(sortedVertices)
-		#print(sortedVertices[i].tolist())
 		vtxX, vtxY = np.array(sortedVertices[i].tolist()).T
-		#print(vtxX)
-		#print(vtxX.tolist())
-		#print(vtxY)
+		
 		tourAxes[i] = plt.subplot(len(tourFiles), 1, i)
-		tourAxes[i].plot(vtxX, vtxY, "b.-")
+		tourAxes[i].plot(vtxX, vtxY, linestyle='-', color='b', markerfacecolor='red', marker='o')
 		tourAxes[i].set_title(tourFiles[i] + " - Weight: " + weight)
+		plt.show()
+	return
 
+def visualizeTree(pointsFile, edgesFile):
+	#read in points
+	vtxFile = open(pointsFile, "rb")
+	vtxCount = re.findall('\d+', vtxFile.readline())
+	vtxCount = int(vtxCount[0])
+
+	#create array of vertices
+	vertices = np.zeros(vtxCount, dtype='i,i').tolist()
+	#print(vertices)
+	for i in range(vtxCount):
+		coords = vtxFile.readline()
+		coords = re.findall('\d+', coords)
+		vertices[i] = (coords[0], coords[1])
+	
+	#read in edges
+	eFile = open(edgesFile)
+	weight = eFile.readline()
+	
+	edges = np.zeros(vtxCount - 1, dtype='i,i').tolist()
+	for i in range(vtxCount - 1):
+		edge = re.findall('\d+', eFile.readline())
+		edges[i] = (int(edge[0]), int(edge[1])) 
+
+	x, y = np.array(vertices).T
+
+	plt.plot(x[np.array(edges).T], y[np.array(edges).T], linestyle='-', color='b', markerfacecolor='red', marker='o') 
+	plt.title(edgesFile + " - Weight: " +  weight)
 	plt.show()
 	return
 
 #create a List of each tour to visualize: Max = 4 tours
-if(len(sys.argv) < 3):
-	print("Error. Usage: python visualizer.py [vertices file] [tour 1 file] [tour 2 file] ... [tour n file]")
+if(len(sys.argv) < 4):
+	print("Error. Usage: python visualizer.py [MST|TSP] [vertices file] [tour 1 file] [tour 2 file] ... [tour n file]")
 	exit
 
-vFile = sys.argv[1]
-tFiles = sys.argv[2:]
-visualizeTour(vFile, tFiles)
+vFile = sys.argv[2]
+tFiles = sys.argv[3:]
+
+if(sys.argv[1] == "MST"):
+	visualizeTree(vFile, tFiles[0])
+else:
+	visualizeTour(vFile, tFiles)
